dce0e2ffe13268fad9cf5431c5ddd30ef85d4c47
ewraj <212665522+ewraj@users.noreply.github.com>
2026-08-17 18:58:44 +0530

    excede: update 2026-08-17 13:28

---
+++ stresstest2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+
+print("Shape:", a.shape)
+print("Mean:", a.mean())
+print("Transpose:")
+print(a.T)
+
+print("Matrix multiplication:")
+print(a @ a)
+
+print("Eigenvalues:")
+print(np.linalg.eigvals(a))
